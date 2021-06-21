e181ed9b6ab571402d377e6179fa6d1adc003eef
ziyicitu <ziyicitu@gmail.com>
2021-06-21 20:04:52 +0800

    添加了标题，小方块可以点击和移动，离开小方块直接回到游戏菜单

--- main.js
+++ main.js
@@ -1,61 +1,176 @@
 /** @type {HTMLCanvasElement} */
 
 var myGameCanvas = document.getElementById("myGameCanvas");
-
-myGameCanvas.height = 600;
-myGameCanvas.width = 800;
-
 var myGameContext = myGameCanvas.getContext("2d");
+
+function gameInit(){
+    myGameCanvas.height = gameHight;
+    myGameCanvas.width = gameWidth;
+    setGetMousePosition();
+}
 
 function drawBackground() {
     myGameContext.fillStyle = "#CDC9C9";
-    myGameContext.fillRect(0, 0, 800, 600);
+    myGameContext.fillRect(0, 0, gameWidth, gameHight);
 }
 
 function drawBlock(position) {
     myGameContext.save();
     myGameContext.fillStyle = "#FFFFFF";
-    myGameContext.fillRect(position.x - 25, position.y - 25, 50, 50);
+    myGameContext.fillRect(position.x - blockSize/2, position.y - blockSize/2, blockSize, blockSize);
     myGameContext.restore();
 }
 
 var XMove = (function () {
     var XDir = "right";
-    return function () {
-        return 
-            console.log(XDir);
-            var x = position.x;
+    return function(){
+            var x = blockPosition.x;
             if (XDir == "right") {
-                x++;
-                if (x > 800) {
+                x += blockSpeed;
+                if (x > (gameWidth - blockSize/2)) {
                     XDir = "left";
                 }
             };
-            if (Xdir == "left") {
-                x--;
-                if (x < 0) {
+            if (XDir == "left") {
+                x -= blockSpeed;
+                if (x < (blockSize/2)) {
                     XDir = "right";
                 }
             }
-        
+            blockPosition.x = x;
+    }
+})();
+
+var YMove = (function(){
+    var YDir = "down";
+    return function(){
+        var y = blockPosition.y;
+        if(YDir == "down"){
+            y += blockSpeed;
+            if(y > (gameHight - blockSize/2)){
+                YDir = "up";
+            }
+        };
+        if(YDir == "up"){
+            y -= blockSpeed;
+            if(y < (blockSize/2)){
+                YDir = "down";
+            }
+        }
+        blockPosition.y = y;
     }
 })();
 
 function blockMove() {
     XMove();
+    YMove();
 }
 
 function drawWhile() {
     myGameContext.clearRect(0, 0, 800, 600);
-    blockMove();
-    drawBackground();
-    drawBlock(position);
+
+    eventUpdata();
+    drawObject();
+
     requestAnimationFrame(drawWhile);
 }
 
-var position = {
-    x: 10,
-    y: 10
+function drawObject(){ //绘图
+    drawBackground();
+    drawBlock(blockPosition);
+    if(gameStatu == "menu"){
+        drawTitle();
+    }  
 }
 
-drawWhile();
+function eventUpdata(){ //事件处理
+    if(gameStatu == "menu"){
+        menuStatu();
+    } else{
+        playingStatu();
+    }
+    
+}
+
+function menuStatu(){ //菜单界面事件处理
+    blockPosition = {
+        x:400,
+        y:400
+    }
+
+    if(isMouseClick() && isMouseInBlock()){
+        gameStatu = "playing";
+    }
+}
+
+function drawTitle(){
+    myGameContext.save();
+    myGameContext.fillStyle = "#FFFFFF";
+    myGameContext.font = "50px sans-serif";
+    myGameContext.fillText("WHITE NOT ALONE", 180, 100);
+    myGameContext.restore();
+}
+
+function playingStatu(){ //游戏界面事件处理
+    blockMove();
+    if(!isMouseInBlock()){
+        gameStatu = "menu";
+    }   
+}
+
+var isMouseClick= (function(){
+    var clicked = false;
+    myGameCanvas.onclick = function(){
+        clicked = true;
+    }
+    return function(){
+        if(clicked == true){
+            clicked = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
+})();
+
+function setGetMousePosition(){
+    myGameCanvas.onmousemove = (event)=>{
+        mousePosition.x = event.offsetX;
+        mousePosition.y = event.offsetY;
+    }
+}
+
+function isMouseInBlock(){
+    if(Math.abs(blockPosition.x - mousePosition.x) > blockSize/2){
+        return false;
+    }
+    
+    if(Math.abs(blockPosition.y - mousePosition.y) > blockSize/2){
+        return false;
+    }
+
+    return true;
+}
+
+var blockPosition = {
+    x: 400,
+    y: 400
+}
+
+var mousePosition = {
+    x:0,
+    y:0
+}
+
+var blockSpeed = 2;
+var blockSize = 100;
+var gameWidth = 800;
+var gameHight = 600;
+var gameStatu = "menu";
+
+function main(){
+    gameInit();
+    drawWhile();
+}
+
+main();
