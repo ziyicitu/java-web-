7f0b08ca616ffd5e5d978954e7d138c01460607b
ziyicitu <ziyicitu@gmail.com>
2021-06-22 15:33:09 +0800

    添加了分数统计和侧边排行榜

--- main.js
+++ main.js
@@ -80,7 +80,17 @@ function drawObject(){ //绘图
     drawBlock(blockPosition);
     if(gameStatu == "menu"){
         drawTitle();
-    }  
+        drawScore();
+        if(isShowLeaderboard()){
+            drawLeaderboard();
+        }
+    }
+
+    if(gameStatu == "playing"){
+        drawScore();
+    }
+
+
 }
 
 function eventUpdata(){ //事件处理
@@ -97,9 +107,30 @@ function menuStatu(){ //菜单界面事件处理
         x:400,
         y:400
     }
+    
+    if(isShowLeaderboard()){
+        leaderboardPosition = {
+            x:570,
+            y:200
+        }
+        leaderboardSize = {
+            width:230,
+            height:390
+        }
+    } else {
+        leaderboardPosition = {
+            x:775,
+            y:200
+        }
+        leaderboardSize = {
+            width:25,
+            height:390
+        }
+    }
 
     if(isMouseClick() && isMouseInBlock()){
         gameStatu = "playing";
+        score = 0;
     }
 }
 
@@ -113,9 +144,35 @@ function drawTitle(){
 
 function playingStatu(){ //游戏界面事件处理
     blockMove();
+    score++;
     if(!isMouseInBlock()){
         gameStatu = "menu";
     }   
+}
+
+function drawLeaderboard(){
+    myGameContext.save();
+    myGameContext.fillStyle = "#BEBEBE";
+    myGameContext.fillRect(570,200,leaderboardSize.width,leaderboardSize.height)
+    myGameContext.restore();
+}
+
+function isShowLeaderboard(){
+    if(mousePosition.x<leaderboardPosition.x || mousePosition.x>leaderboardPosition.x+leaderboardSize.width){
+        return false;
+    }
+    if(mousePosition.y<leaderboardPosition.y || mousePosition.y>leaderboardPosition.y+leaderboardSize.height){
+        return false;
+    }
+    return true;
+}
+
+function drawScore(){
+    myGameContext.save();
+    myGameContext.fillStyle = "#FFFFFF";
+    myGameContext.font = "50px sans-serif";
+    myGameContext.fillText(score, 400-(Math.floor(Math.log10(score)))*25, 250);
+    myGameContext.restore();
 }
 
 var isMouseClick= (function(){
@@ -162,11 +219,22 @@ var mousePosition = {
     y:0
 }
 
+var leaderboardPosition = {
+    x:570,
+    y:200
+}
+
+var leaderboardSize = {
+    width:225,
+    height:390
+}
+
 var blockSpeed = 2;
 var blockSize = 100;
 var gameWidth = 800;
 var gameHight = 600;
 var gameStatu = "menu";
+var score = 0;
 
 function main(){
     gameInit();
